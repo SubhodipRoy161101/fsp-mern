3b166a85260efd05cac4e015e623e6198ec41d77
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2023-06-30 13:33:00 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Logo from "../assets/logo.png";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="" />
+      <a href="/">Home</a>
+    </header>
+  );
+};
+
+export default Header;
